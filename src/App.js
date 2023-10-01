4e954dd58e08af402ae28c1c00815500ab7c5913
GavishkaChamindu <gavichamindu@gmail.com>
2023-10-01 12:53:22 +0530

    complete all pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import Banner from "./pages/banner/Banner";
+import Footer from "./component/footer/Footer";
+import HeaderPage from "./HeaderPage/HeaderPage";
+import HomePage from "./pages/HomePage/HomePage";
 import Navbar from "./component/Navbar/Navbar";
+import Question from "./pages/question/Question";
 
 function App() {
   return (
     <div>
+
     
 <Navbar/>
+
+    <HeaderPage/>
+
+    <HomePage/>
+    <Question/>
+<Banner/>
+
+<Footer/>
+
     </div>
   );
 }
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,28 +2,27 @@ import "./navbar.css";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import Image from "./3 9.png";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { BiLogIn } from "react-icons/bi";
 import { HiOutlineLogout } from "react-icons/hi";
 import { RiQuestionnaireLine } from "react-icons/ri";
 
-export default function CustomNavbar() {
+  export default function CustomNavbar() {
   return (
     <div>
       <Navbar bg="light" data-bs-theme="light">
         <Container>
           <div className="nav-left">
             <div className="navbar-image">
-              <img
-                src=""
-                alt=""
-                className="n-image"
-              />
+
+              <img src={Image} alt="" className="n-image" />
+             
             </div>
             <div className="headers">
-              <span className="online">Online</span>{" "}
-              <h1 className="qr-generator">QR Generator</h1>
+             
+              
             </div>
           </div>
 
@@ -58,6 +57,7 @@ export default function CustomNavbar() {
           </div>
         </Container>
       </Navbar>
+      <br /> <br />
     </div>
   );
 }
